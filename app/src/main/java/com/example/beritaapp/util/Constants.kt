e62439e83d65ec4daab0bb41e5677f049d012ea1
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-08-02 16:27:18 +0700

    change API account

--- app/src/main/java/com/example/beritaapp/util/Constants.kt
+++ app/src/main/java/com/example/beritaapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.beritaapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "cb7da42649fa45d68c454c47d31cda47"
+        const val API_KEY = "b4b4b89579134c02897523835b4c0be6"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
