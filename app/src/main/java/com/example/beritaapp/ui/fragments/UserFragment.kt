d418843186e8400e003ae5036ac5bcc922bf3f29
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-08-09 14:21:02 +0700

    fix login and register activity

--- app/src/main/java/com/example/beritaapp/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/example/beritaapp/ui/fragments/UserFragment.kt
@@ -1,22 +1,22 @@
 package com.example.beritaapp.ui.fragments
 
 import android.app.AlertDialog
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.beritaapp.R
 import com.example.beritaapp.ui.*
 import com.google.firebase.auth.FirebaseAuth
-import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.fragment_user.*
+import java.io.ByteArrayOutputStream
+
 
 class UserFragment : Fragment(R.layout.fragment_user) {
 
@@ -32,31 +32,51 @@ class UserFragment : Fragment(R.layout.fragment_user) {
         if(auth.currentUser == null){
            intent()
         }
+        icLogout.bringToFront()
         icLogout.setOnClickListener {
             AlertDialog.Builder(context)
                 .setTitle("Konfirmasi")
                 .setMessage("Apakah anda ingin keluar?")
                 .setPositiveButton("Keluar", DialogInterface.OnClickListener { dialogInterface, i ->
-                    deleteProduct()
+                    signOut()
                 })
                 .setNegativeButton("Batal", DialogInterface.OnClickListener { dialogInterface, i ->
                     dialogInterface.dismiss()
                 })
+                .show()
         }
         btnUbahUser.setOnClickListener {
-            startActivity(Intent(context, EditUser::class.java))
+            val bitmap = imgAccount.drawable.toBitmap()
+            val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            val byteArray: ByteArray = stream.toByteArray()
+
+            val intent = Intent(context, EditUser::class.java)
+            intent.putExtra("edit", "edit")
+            intent.putExtra("picture", byteArray)
+            intent.putExtra("name", auth.currentUser?.displayName)
+            startActivity(intent)
         }
 
     }
 
-    private fun deleteProduct() {
+    private fun signOut() {
         auth.signOut()
         Toast.makeText(context, "successfully logout", Toast.LENGTH_LONG).show()
         intent()
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
+        setProfile()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        setProfile()
+    }
+
+    private fun setProfile() {
         val user = auth.currentUser
         tvUsername.text = user?.displayName
         context?.let {
